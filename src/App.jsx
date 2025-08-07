12a0496d7a8ad609421475792d48635036cea3e9
=Hammam-AbuAlrob <=Cohammam@gmail.com>
2025-08-07 23:06:17 +0300

    init proj

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+/*
+:نستطيع تنزيل المكتبات التي سنتاجها في المشروع مرة واحدة عن طريق كتابة
+npm i bootstrap axios react-router-dom react-hook-form
+react-router-dom ==> spa
+react toastify
+*/
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer.jsx"
+import Navbar from "./components/navbar/Navbar.jsx"
+import Home from "./pages/home/Home.jsx"
+import Create from "./pages/user/Create.jsx"
+import About from "./pages/about/About.jsx"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />      
+        <Route path='/users' element={<Home/>} />
+        <Route path='/add' element={<Create/>} />
+        <Route path='/about' element={<About/>} />
+
+      </Routes>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
